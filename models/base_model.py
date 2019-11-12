abf1bf186a2d7bcf99543ae2c3df16bd4ddf85e9
cherjiosclear <slopez0286@gmail.com>
2019-11-12 22:07:52 +0000

    adding Base model 3

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,9 @@ This is a BaseModel module for base class
 """
 
 
-import models
+import models 
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -16,24 +16,36 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initiate class constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
-    def  __str__(self):
+    def __str__(self):
         """Return class name, id, dict"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.to_dict)
+        return "[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.to_dict)
 
     def save(self):
         """Updates 'updated_at' with current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dict containing all keys/values of __dict__"""
-        my_dict = {}
-        my_dict["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__items():
-            if isinstance(value, datetime) is True:
-                value = value.isoformat()
-            my_dict[key] = value    
-        return my_dict
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = type(self).__name__
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['created_at'] = self.created_at.isoformat()
+        return new_dict
