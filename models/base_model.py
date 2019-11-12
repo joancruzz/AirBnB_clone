a1984a46300bb3b77f78889b73f37026d72420b7
cherjiosclear <slopez0286@gmail.com>
2019-11-12 23:33:24 +0000

    Fixing base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,32 +10,33 @@ import models
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
     """Represents a base class"""
 
     def __init__(self, *args, **kwargs):
-        """Initiate class constructor"""
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        """ initialize an instance potentially with a dictionary argument"""
+        if kwargs and len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime
+                            (v, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k == "__class__":
+                    pass
                 else:
-                    self.__dict__[key] = kwargs[key]
-        else:
+                    setattr(self, k, v)
+        if "id" not in kwargs:
             self.id = str(uuid.uuid4())
+        if "created_at" not in kwargs:
             self.created_at = datetime.now()
+        if "updated_at" not in kwargs:
             self.updated_at = datetime.now()
-            models.storage.new(self)
+        if not kwargs or len(kwargs) == 0:
+            models.storage.new(self) 
 
     def __str__(self):
-        """Return class name, id, dict"""
-        return "[{}] ({}) {}".format(type(self).__name__,
-                                     self.id, self.to_dict)
+        """ overwrite string special method """
+        out = "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        return out  
 
     def save(self):
         """Updates 'updated_at' with current datetime"""
@@ -45,7 +46,7 @@ class BaseModel:
     def to_dict(self):
         """Returns a dict containing all keys/values of __dict__"""
         new_dict = self.__dict__.copy()
-        new_dict['__class__'] = type(self).__name__
+        new_dict['__class__'] = self.__class__.__name__
         new_dict['updated_at'] = self.updated_at.isoformat()
         new_dict['created_at'] = self.created_at.isoformat()
         return new_dict
